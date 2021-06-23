79bbbbaa5c3268733fcab9fe7c193429436077a6
estherzulema <estherzulema10@gmail.com>
2021-06-23 02:25:30 -0400

    BACKEND

--- todo/backend/src/api/todo/todoService.js
+++ todo/backend/src/api/todo/todoService.js
@@ -1,7 +1,34 @@
-const Todo = require('./todo');
+const Todo = require('./todo'); 
 
-Todo.methods(['get','post','put','delete']);
+/**
+ * @api {get} /task List all tasks
+ * @apiGroup Tasks
+ *
+ *
+ * @apiSuccess {Object[]} tasks Task's list
+ * @apiSuccess {Number} tasks.id Task id
+ * @apiSuccess {String} tasks.title Task title
+ * @apiSuccess {Boolean} tasks.done Task is done ?
+ * @apiSuccess {Date} tasks.update_at Update's date
+ * @apiSuccess {Date} tasks.created_at Register's date
+ * @apiSuccessExample {json} Success
+ *     HTTP/1.1 200 OK
+ *     [{
+ *       "id": 1,
+ *       "title": "Study",
+ *       "done": false,
+ *       "update_at": "2020-02-10T15:46:51.778Z",
+ *       "created_at":"2020-02-10T15:46:51.778Z"
+ *     }]
+ *
+ *
+ * @apiErrorExample {json} Error
+ *     HTTP/1.1 500 Internal Server Error
+ *     
+ */
 
+Todo.methods(['get', 'post', 'put', 'delete']); 
 Todo.updateOptions({runValidators: true});
+
 
 module.exports = Todo;
