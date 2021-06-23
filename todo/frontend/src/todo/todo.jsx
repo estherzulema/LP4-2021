15cdcc12a85fc0fb370242a483f378f56be61a02
estherzulema <estherzulema10@gmail.com>
2021-06-23 02:00:19 -0400

    -TODOLIST-

--- todo/frontend/src/todo/todo.jsx
+++ todo/frontend/src/todo/todo.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PageHeader from '../tamplete/pageHearder';
+import PageHeader from '../template/pageHeader';
 import axios from 'axios';
 
 import TodoForm from './todoForm';
